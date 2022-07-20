d0305ed04a310fd23a4412d84ec1b88060df9857
Mohamed Ali <eng.ayaty@gmail.com>
2022-07-20 08:10:19 +0200

    little changes to simplify the card module and some CMake fixes

--- Card/card.c
+++ Card/card.c
@@ -4,50 +4,53 @@
 
 EN_cardError_t getCardHolderName(ST_cardData_t *cardData){
 
-    uint8_t temp [26];  // acts as a buffer to check the user input
     printf("Please enter the card name: ");
-    gets(temp);
-    uint32_t i = strlen(temp);
-    if( i > 24 ||  i < 20  ){
+    gets(cardData->cardHolderName);
+    uint32_t i = strlen(cardData->cardHolderName);
 
+    if( i > 24 ||  i < 20  ){
         return WRONG_NAME;
     }
-    strcpy(cardData->cardHolderName , temp);
 
     return OK;
-
-    
-    
 }
+
 EN_cardError_t getCardExpiryDate(ST_cardData_t *cardData){
 
-    uint8_t temp [7];
     printf("Please enter the Expiry date as 'MM/YY' : ");
+    gets(cardData->cardExpirationDate);
+    uint32_t i = strlen(cardData->cardExpirationDate);
 
-    gets(temp);
-    uint32_t i = strlen(temp);
     if( i != 5  ){
-
         return WRONG_EXP_DATE;
     }
-    strcpy(cardData->cardExpirationDate , temp);
 
     return OK;
-
-    
 }
+
 EN_cardError_t getCardPAN(ST_cardData_t *cardData){
 
-    uint8_t temp [21];
     printf("Please enter the card PAN: ");
+    gets(cardData ->primaryAccountNumber);
+    uint32_t i = strlen(cardData ->primaryAccountNumber);
 
-    gets(temp);
-    uint32_t i = strlen(temp);
     if( i > 19 ||  i < 16   ){
-
         return WRONG_PAN;
     }
-    strcpy(cardData ->primaryAccountNumber , temp);
 
     return OK ;
+}
+
+
+int main (void){
+
+
+    ST_cardData_t buyer;
+    EN_cardError_t error = getCardHolderName(&buyer);
+
+    if(error == WRONG_NAME){
+        printf("Wrong name ");
+    } else{
+        printf("the name is correct");
+    }
 }
